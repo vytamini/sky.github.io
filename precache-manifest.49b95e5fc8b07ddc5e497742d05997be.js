08a944d3ffc509d1a921c763f80f33001bd5da3d
vytamini <vytamini1@gmail.com>
2019-01-09 17:28:04 +0100

    initial commit

---
+++ precache-manifest.49b95e5fc8b07ddc5e497742d05997be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "58ff18764d4bddd66867",
+    "url": "/static/css/main.b0e34a28.chunk.css"
+  },
+  {
+    "revision": "58ff18764d4bddd66867",
+    "url": "/static/js/main.58ff1876.chunk.js"
+  },
+  {
+    "revision": "24238c8751c239a5a46f",
+    "url": "/static/js/1.24238c87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9dcb9a5d6ed4a126b54e379f2d2abcd",
+    "url": "/static/media/apel.d9dcb9a5.png"
+  },
+  {
+    "revision": "c0d200ea36f8e401ff7b7394884acea0",
+    "url": "/static/media/chokolade.c0d200ea.png"
+  },
+  {
+    "revision": "2dcb676435e6137fd6b9c58c3ff4d10e",
+    "url": "/static/media/karneval.2dcb6764.png"
+  },
+  {
+    "revision": "6c19101d88d1a21f0e9b7aee272fca8c",
+    "url": "/index.html"
+  }
+];
